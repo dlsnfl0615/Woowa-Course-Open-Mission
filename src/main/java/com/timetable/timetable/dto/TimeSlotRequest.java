c020b306a999f91ae9667793743792464221b4a1
dlsnfl0615 <hyunseo0750@gmail.com>
2025-11-17 00:02:13 +0900

    feat: TimeSlot Dto의 시작 시간과 종료 시간을 LocalTime으로 변경

--- src/main/java/com/timetable/timetable/dto/TimeSlotRequest.java
+++ src/main/java/com/timetable/timetable/dto/TimeSlotRequest.java
@@ -1,17 +1,46 @@
 package com.timetable.timetable.dto;
 
+import com.timetable.timetable.domain.Day;
 import com.timetable.timetable.domain.TimeSlot;
 
+import java.time.LocalTime;
+
 public record TimeSlotRequest(
-        String day,
-        String start,
-        String end
+        Day day,
+        LocalTime start,
+        LocalTime end
 ) {
     public static TimeSlotRequest from(TimeSlot slot) {
         return new TimeSlotRequest(
-                slot.getDay().name(),
-                slot.getStart().toString(),
-                slot.getEnd().toString()
+                slot.getDay(),
+                slot.getStart(),
+                slot.getEnd()
         );
+    }
+
+    public String convertToGridPosition() {
+        int column = day.ordinal() + 2;
+        int[] row = getGridRow();
+
+        return String.format("grid-column: %d; grid-row: %d / %d", column, row[0], row[1]);
+    }
+
+    public int[] getGridRow() {
+        int startRow = calculateRow(start);
+        int endRow = calculateRow(end);
+
+        System.out.printf("start: %d, end: %d\n", startRow, endRow);
+
+        return new int[]{startRow, endRow};
+    }
+
+    private int calculateRow(LocalTime time) {
+        int hour = time.getHour();
+        int minute = time.getMinute();
+
+        int startRow = (hour - 9) * 2 + 2;
+        startRow += minute / 30;
+
+        return startRow;
     }
 }
