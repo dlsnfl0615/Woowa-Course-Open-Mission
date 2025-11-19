a0a749bd6d3cbf9a71c93ee111b4ff96e1bfb224
dlsnfl0615 <hyunseo0750@gmail.com>
2025-11-19 20:44:35 +0900

    refactor: 전체 강의 목록을 상태로 가지도록 수정

--- src/main/java/com/timetable/timetable/service/TimeTableMaker.java
+++ src/main/java/com/timetable/timetable/service/TimeTableMaker.java
@@ -11,14 +11,16 @@ import java.util.List;
 public class TimeTableMaker {
     private final RegisterWizard registerWizard;
     private final LectureMatcher lectureMatcher;
+    private final List<Lecture> allLectures;
 
-    public TimeTableMaker(RegisterWizard registerWizard, LectureMatcher lectureMatcher) {
+    public TimeTableMaker(RegisterWizard registerWizard, LectureMatcher lectureMatcher, List<Lecture> allLectures) {
         this.registerWizard = registerWizard;
         this.lectureMatcher = lectureMatcher;
+        this.allLectures = allLectures;
     }
 
-    public List<TimeTable> makeTimeTable(List<String> lectureCodes, List<Lecture> availableLectures, List<Day> noLectureDays) {
-        List<Lecture> lecturesToRegister = lectureMatcher.findLecturesWithCode(lectureCodes, availableLectures);
+    public List<TimeTable> makeTimeTable(List<String> lectureCodes, List<Day> noLectureDays) {
+        List<Lecture> lecturesToRegister = lectureMatcher.findLecturesWithCode(lectureCodes, allLectures);
         return registerWizard.makeTimeTable(lecturesToRegister, noLectureDays);
     }
 }
