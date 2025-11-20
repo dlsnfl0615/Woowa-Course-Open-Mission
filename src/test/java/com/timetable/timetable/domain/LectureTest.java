89a3a6ea84faca40ca6374af659fd9e41c29a7fa
dlsnfl0615 <hyunseo0750@gmail.com>
2025-11-20 22:19:38 +0900

    fix: 클래스 정의 변경에 맞춰 객체 선언 인자 전달 변경

--- src/test/java/com/timetable/timetable/domain/LectureTest.java
+++ src/test/java/com/timetable/timetable/domain/LectureTest.java
@@ -9,12 +9,12 @@ import static org.assertj.core.api.Assertions.*;
 
 class LectureTest {
     List<TimeSlot> addedLectureTimes = List.of(new TimeSlot(Day.MON, LocalTime.of(8, 0), LocalTime.of(9, 0)));
-    Lecture addedLecture = new Lecture("자바", "최진우", "CSC-1234", addedLectureTimes);
+    Lecture addedLecture = new Lecture("자바", "최진우", "CSC-1234", "강의실1",addedLectureTimes, false);
 
     @Test
     void 분반_시간_중복없음() {
         List<TimeSlot> lectureTimes1 = List.of(new TimeSlot(Day.MON, LocalTime.of(10, 30), LocalTime.of(11, 0)));
-        Lecture lecture1 = new Lecture("C++", "최진우", "CSC-0987", lectureTimes1);
+        Lecture lecture1 = new Lecture("C++", "최진우", "CSC-0987", "강의실1",lectureTimes1, false);
 
         assertThat(addedLecture.overlapsTimeWith(lecture1)).isFalse();
     }
@@ -22,16 +22,16 @@ class LectureTest {
     @Test
     void 분반_시간_중복있음() {
         List<TimeSlot> lectureTimes1 = List.of(new TimeSlot(Day.MON, LocalTime.of(8, 30), LocalTime.of(10, 0)));
-        Lecture lecture1 = new Lecture("자바", "최진우", "CSC-5678", lectureTimes1);
+        Lecture lecture1 = new Lecture("자바", "최진우", "CSC-5678", "강의실1",lectureTimes1, false);
 
         List<TimeSlot> lectureTimes2 = List.of(new TimeSlot(Day.MON, LocalTime.of(8, 30), LocalTime.of(10, 0)));
-        Lecture lecture2 = new Lecture("", "최진우", "CSC-5678", lectureTimes2);
+        Lecture lecture2 = new Lecture("", "최진우", "CSC-5678", "강의실1",lectureTimes2, false);
 
         List<TimeSlot> lectureTimes3 = List.of(new TimeSlot(Day.MON, LocalTime.of(8, 30), LocalTime.of(10, 0)));
-        Lecture lecture3 = new Lecture("c++", "최진우", "CSC-1000", lectureTimes3);
+        Lecture lecture3 = new Lecture("c++", "최진우", "CSC-1000", "강의실1",lectureTimes3, false);
 
         List<TimeSlot> lectureTimes4 = List.of(new TimeSlot(Day.MON, LocalTime.of(8, 30), LocalTime.of(10, 0)));
-        Lecture lecture4 = new Lecture("c++", "최진우", "CSC-1000", lectureTimes4);
+        Lecture lecture4 = new Lecture("c++", "최진우", "CSC-1000", "강의실1",lectureTimes4, false);
 
         assertThat(addedLecture.overlapsTimeWith(lecture1)).isTrue();
         assertThat(addedLecture.overlapsTimeWith(lecture2)).isTrue();
