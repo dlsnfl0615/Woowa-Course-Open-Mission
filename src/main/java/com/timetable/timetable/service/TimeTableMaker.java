44032353b002899441351256104da072605f5e76
dlsnfl0615 <hyunseo0750@gmail.com>
2025-11-20 22:17:29 +0900

    refactor: 기존 RegisterWizard를 인터페이스로 변경한 객체 사용

--- src/main/java/com/timetable/timetable/service/TimeTableMaker.java
+++ src/main/java/com/timetable/timetable/service/TimeTableMaker.java
@@ -1,26 +1,26 @@
 package com.timetable.timetable.service;
 
 import com.timetable.timetable.domain.Lecture;
-import com.timetable.timetable.domain.LectureMatcher;
-import com.timetable.timetable.domain.RegisterWizard;
+import com.timetable.timetable.util.LectureMatcher;
+import com.timetable.timetable.domain.DFSTimeTableGenerator;
 import com.timetable.timetable.domain.TimeTable;
 import com.timetable.timetable.domain.Day;
 
 import java.util.List;
 
 public class TimeTableMaker {
-    private final RegisterWizard registerWizard;
+    private final DFSTimeTableGenerator DFSTimeTableGenerator;
     private final LectureMatcher lectureMatcher;
     private final List<Lecture> allLectures;
 
-    public TimeTableMaker(RegisterWizard registerWizard, LectureMatcher lectureMatcher, List<Lecture> allLectures) {
-        this.registerWizard = registerWizard;
+    public TimeTableMaker(DFSTimeTableGenerator DFSTimeTableGenerator, LectureMatcher lectureMatcher, List<Lecture> allLectures) {
+        this.DFSTimeTableGenerator = DFSTimeTableGenerator;
         this.lectureMatcher = lectureMatcher;
         this.allLectures = allLectures;
     }
 
     public List<TimeTable> makeTimeTable(List<String> lectureCodes, List<Day> noLectureDays) {
         List<Lecture> lecturesToRegister = lectureMatcher.findLecturesWithCode(lectureCodes, allLectures);
-        return registerWizard.makeTimeTable(lecturesToRegister, noLectureDays);
+        return DFSTimeTableGenerator.generate(lecturesToRegister, noLectureDays);
     }
 }
